6d6dfa4d52b89a41a446317b2665882408ababaf
noname <Vous@exemple.com>
2025-02-12 16:57:35 +0100

    minor fix in output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "lxc_highest_id_value" {
   description = "highest lxc id existing on proxmox node"
-  value = tonumber(trimspace(data.local_file.highest_lxc_id[*].content))
+  value = data.local_file.highest_lxc_id.count > 0 ? tonumber(trimspace(data.local_file.highest_lxc_id[*].content)) : null
 }
