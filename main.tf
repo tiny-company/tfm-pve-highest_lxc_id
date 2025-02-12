7108a47d0f3fb88a71d2eb2f7facee3bb75c16ba
noname <Vous@exemple.com>
2025-02-12 09:39:00 +0100

    fixing order by removing lifecycle

--- main.tf
+++ main.tf
@@ -13,9 +13,6 @@ resource "null_resource" "import_script_dependencies" {
   provisioner "local-exec" {
     command = "virtualenv -p ${var. python_version} ${path.module}/venv && . ${path.module}/venv/bin/activate && pip install -r ${path.module}/scripts/requirements.txt"
   }
-  lifecycle {
-    ignore_changes = all
-  }
 }
 
 resource "null_resource" "get_highest_lxc_id" {
