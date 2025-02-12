7da69cfc149022a8cbc5cfe54c367a63a5ec62ef
noname <Vous@exemple.com>
2025-02-12 09:36:11 +0100

    adding documentation to the python script, try to fix error related to execution order

--- main.tf
+++ main.tf
@@ -8,12 +8,6 @@
 # - terraform version : OpenTofu v1.9.0
 # ------------------------------------------------------------------
 
-## avoid error related to file missing when read by data 
-resource "local_file" "create_highest_lxc_id" {
-  content = ""
-  filename = "${path.module}/highest_lxc_id.txt"
-}
-
 resource "null_resource" "import_script_dependencies" {
   triggers  =  { always_run = "${timestamp()}" }
   provisioner "local-exec" {
@@ -22,30 +16,24 @@ resource "null_resource" "import_script_dependencies" {
   lifecycle {
     ignore_changes = all
   }
-  depends_on = [local_file.create_highest_lxc_id]
 }
 
 resource "null_resource" "get_highest_lxc_id" {
-  triggers  =  { always_run = "${timestamp()}" }
   provisioner "local-exec" {
     command = "${path.module}/venv/bin/python ${path.module}/scripts/get_highest_lxc_id.py > ${path.module}/highest_lxc_id.txt"
     environment = {
       PROXMOX_API_HOST = "${var.proxmox_api_host}"
-      PROXMOX_API_NODENAME = "${var.proxmox_api_nodename}"
       PROXMOX_API_USERNAME = "${var.proxmox_api_username}"
       PROXMOX_API_TOKENNAME = "${var.proxmox_api_tokenname}"
       PROXMOX_API_TOKENVALUE = "${var.proxmox_api_tokenvalue}"
     }
   }
-  lifecycle {
-    ignore_changes = all
-  }
-  depends_on = [local_file.create_highest_lxc_id, null_resource.import_script_dependencies]
+  depends_on = [null_resource.import_script_dependencies]
 }
 
 data "local_file" "highest_lxc_id" {
   filename = "${path.module}/highest_lxc_id.txt"
-  depends_on = [null_resource.get_highest_lxc_id, local_file.create_highest_lxc_id]
+  depends_on = [null_resource.get_highest_lxc_id]
 }
 
 ## delete venv folder (keep env clean)
--- scripts/get_highest_lxc_id.py
+++ scripts/get_highest_lxc_id.py
@@ -5,26 +5,62 @@ from dotenv import load_dotenv
 from proxmoxer import ProxmoxAPI
 
 
-def instanciate_proxmox():
+def instanciate_proxmox(PROXMOX_API_HOST: str, PROXMOX_API_USERNAME: str, PROXMOX_API_TOKENNAME: str, PROXMOX_API_TOKENVALUE: str) -> ProxmoxAPI:
+    """
+    instanciate proxmox using given parameters loaded from env var
 
-    load_dotenv()
-    PROXMOX_API_HOST = os.getenv('PROXMOX_API_HOST')
-    PROXMOX_API_NODENAME = os.getenv('PROXMOX_API_NODENAME')
-    PROXMOX_API_USERNAME = os.getenv('PROXMOX_API_USERNAME')
-    PROXMOX_API_TOKENNAME = os.getenv('PROXMOX_API_TOKENNAME')
-    PROXMOX_API_TOKENVALUE = os.getenv('PROXMOX_API_TOKENVALUE')
-    proxmox = ProxmoxAPI(
+    Parameters:
+    - PROXMOX_API_HOST : proxmox host address
+    - PROXMOX_API_USERNAME : proxmox username used to connect to pve
+    - PROXMOX_API_TOKENNAME : proxmox token name used to connect to pve
+    - PROXMOX_API_TOKENVALUE : proxmox token value used to connect to pve
+
+    Returns:
+    proxmox: instance of ProxmoxAPI 
+
+    Raises:
+    ValueError: If ProxmoxAPI instanciation failed
+    """
+    try:
+      proxmox = ProxmoxAPI(
         PROXMOX_API_HOST, user=PROXMOX_API_USERNAME, token_name=PROXMOX_API_TOKENNAME, token_value=PROXMOX_API_TOKENVALUE, verify_ssl=False
-    )
-    return proxmox
+      )
+      return proxmox
+    except ValueError as e:
+      print(f"Error: {e}")
+    
+def get_highest_lxc_id(proxmox) -> int:
+    """
+    get the highest lxc id on a proxmox for all nodes
 
-def get_highest_lxc_id(proxmox):
+    Parameters:
+    - proxmox : instance of ProxmoxAPI
 
-    containers = proxmox.nodes("proxmox").lxc.get()
-    highest_id = max(container['vmid'] for container in containers)
-    return highest_id
+    Returns:
+    highest_id: highest lxc id as integer 
+
+    Raises:
+    ValueError: If request to proxmox failed
+    """
+    try:
+      containers = proxmox.nodes("proxmox").lxc.get()
+      highest_id = int(max(container['vmid'] for container in containers))
+      return highest_id
+    except ValueError as e:
+      print(f"Error: {e}")
 
 if __name__ == "__main__":
-    proxmox = instanciate_proxmox()
-    highest_id = get_highest_lxc_id(proxmox)
-    print(highest_id)
+
+  ## load parameters from env var
+  load_dotenv()
+  PROXMOX_API_HOST = os.getenv('PROXMOX_API_HOST')
+  PROXMOX_API_USERNAME = os.getenv('PROXMOX_API_USERNAME')
+  PROXMOX_API_TOKENNAME = os.getenv('PROXMOX_API_TOKENNAME')
+  PROXMOX_API_TOKENVALUE = os.getenv('PROXMOX_API_TOKENVALUE')
+
+  ## instanciate proxmox conn
+  proxmox = instanciate_proxmox(PROXMOX_API_HOST, PROXMOX_API_USERNAME, PROXMOX_API_TOKENNAME, PROXMOX_API_TOKENVALUE)
+  
+  ## get the highest lxc container id
+  highest_id = get_highest_lxc_id(proxmox)
+  print(highest_id)
